e16c39ced2c6414da8202f9e2d3b97573c4ac030
Duc <ducvt1310E@gmail.com>
2025-09-24 20:58:38 +0700

    #update add error-not-found component; remove unused layout component

---
+++ src/app/common/component/error-not-found/error-not-found.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-not-found',
+  imports: [],
+  templateUrl: './error-not-found.html',
+  styleUrl: './error-not-found.scss'
+})
+export class ErrorNotFound {
+
+}
