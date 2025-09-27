cd4acad6ecd2abac01df80cb73cf644ef4ffc4b2
Duc <ducvt1310E@gmail.com>
2025-09-27 11:32:37 +0700

    #update enhance todo list component with task status display and CRUD functionality

--- src/app/common/constant/constant.ts
+++ src/app/common/constant/constant.ts
@@ -20,3 +20,10 @@ export enum TASK_STATUS {
   IN_PROGRESS = 'In Progress',
   COMPLETED = 'Completed',
 }
+
+export const TASK_STATUS_COLORS: Record<TASK_STATUS, string> = {
+  [TASK_STATUS.TODO]: 'bg-[#A9A9A9] text-white',
+  [TASK_STATUS.PENDING]: 'bg-[#FFC300] text-black',
+  [TASK_STATUS.IN_PROGRESS]: 'bg-[#3498DB] text-white',
+  [TASK_STATUS.COMPLETED]: 'bg-[#2ECC71] text-white',
+};
--- src/app/modules/todo-list/todo-list.component.ts
+++ src/app/modules/todo-list/todo-list.component.ts
@@ -2,16 +2,18 @@ import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { catchError, Subject, takeUntil, tap } from 'rxjs';
+import { catchError, concatMap, Subject, takeUntil, tap } from 'rxjs';
 import { CreateTodoItemDTO, TodoItem } from './todo-list.types';
 import { TodoListService } from './todo-list.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
 import { AddEditTaskComponent } from './add-edit-task/add-edit-task.component';
+import { TASK_STATUS, TASK_STATUS_COLORS } from '../../common/constant/constant';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [NzIconModule, NzButtonModule, NzCardModule, NzModalModule],
+  imports: [NzIconModule, NzButtonModule, NzCardModule, NzModalModule, NgClass],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.scss',
 })
@@ -26,11 +28,22 @@ export class TodoList implements OnInit, OnDestroy {
     private _modalService: NzModalService
   ) {}
 
-  ngOnInit(): void {}
+  // Lifecycle hooks
+  ngOnInit(): void {
+    this.handleGetAllTasks().subscribe();
+  }
 
   ngOnDestroy(): void {
     this._unsubscribeAll.next();
     this._unsubscribeAll.complete();
+  }
+
+  // Data Fetching
+  handleGetAllTasks() {
+    return this._todoService.getAllTasks().pipe(
+      takeUntil(this._unsubscribeAll),
+      tap((tasks) => this.todoList.set(tasks))
+    );
   }
 
   // Add a new task
@@ -61,11 +74,74 @@ export class TodoList implements OnInit, OnDestroy {
       .createTask(newTask)
       .pipe(
         takeUntil(this._unsubscribeAll),
-        tap((value) => {
-          console.log(value);
+        tap(() => {
           this._messageService.success('Task added successfully');
-        })
+        }),
+        concatMap(() => this.handleGetAllTasks())
       )
       .subscribe();
+  }
+
+  // Edit task
+  handleEditTask(id: string): void {
+    const modal = this._modalService.create({
+      nzTitle: 'Edit Task',
+      nzContent: AddEditTaskComponent,
+      nzFooter: null,
+      nzData: { id },
+    });
+
+    modal.afterClose.subscribe((value) => {
+      if (value) {
+        this.editTask(id, value);
+      }
+    });
+  }
+
+  editTask(id: string, data: CreateTodoItemDTO): void {
+    const updatedTask: Partial<TodoItem> = {
+      name: data.name,
+      description: data.description,
+      status: data.status,
+      updatedAt: new Date().valueOf(),
+    };
+
+    this._todoService
+      .editTask(id, updatedTask)
+      .pipe(
+        takeUntil(this._unsubscribeAll),
+        tap(() => {
+          this._messageService.success('Task edited successfully');
+        }),
+        concatMap(() => this.handleGetAllTasks())
+      )
+      .subscribe();
+  }
+
+  // Handle delete task
+  handleDeleteTask(id: string): void {
+    this._modalService.warning({
+      nzTitle: 'Are you sure you want to delete this task?',
+      nzContent: 'This action cannot be undone.',
+      nzOnOk: () => this.deleteTask(id),
+    });
+  }
+
+  deleteTask(id: string): void {
+    this._todoService
+      .deleteTask(id)
+      .pipe(
+        takeUntil(this._unsubscribeAll),
+        tap(() => {
+          this._messageService.success('Task deleted successfully');
+        }),
+        concatMap(() => this.handleGetAllTasks())
+      )
+      .subscribe();
+  }
+
+  // Utility function
+  statusColor(status: string): string {
+    return TASK_STATUS_COLORS[status as TASK_STATUS] || 'bg-gray-500 text-white';
   }
 }
